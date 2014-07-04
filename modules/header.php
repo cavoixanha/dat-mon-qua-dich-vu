3bbe3a26789511b06736c676432fe8c77efe8e9c
vannam1993cse@gmail.com <vannam1993cse@gmail.com@f3eee216-bba2-e6f0-d065-ffc90344fca8>
2014-07-04 15:40:38 +0000

    Update header.php

--- modules/header.php
+++ modules/header.php
@@ -1,49 +1,52 @@
 <link href="../css/header.css" rel="stylesheet" type="text/css">
-
+<script type="text/javascript">
+	function thongbao()
+	{
+		alert("sss");
+		}
+</script>
 <div id="nav_top">
-  <div class="s960" >
-    <ul id="text_nav">
-      <li><a href="index.php">Home</a></li>
-      <li><a href="#">About</a></li>
-      <!--      <li><a href="#">Liên Hệ</a></li>
+	<div class="s960">
+		<ul id="text_nav">
+			<li><a href="index.php">Home</a></li>
+			<li><a href="#">About</a></li>
+			<!--      <li><a href="#">Liên Hệ</a></li>
       <li><a href="Login.php">Đăng Nhập</a></li>-->
-    </ul>
-    <div style="float:right;">
-      <div style="float:left; padding-top:2px;">
-        <input class="rounded_corner" type="text" name="txttimkiem" />
-      </div>
-      <div style="float:left;margin-left:-55px;z-index:10;">
-        <button id="btntimkiem" type="button" onclick="thongbao()"></button>
-      </div>
-    </div>
-  </div>
+		</ul>
+		<div style="float: right;">
+			<div style="float: left; padding-top: 2px;">
+				<input class="rounded_corner" type="text" name="txttimkiem" />
+			</div>
+			<div style="float: left; margin-left: -55px; z-index: 10;">
+				<button id="btntimkiem" type="button" onclick="thongbao()"></button>
+			</div>
+		</div>
+	</div>
 </div>
 <!--<div class="clear_float"></div>-->
 <!-- Banner Nha Hang-->
 <div id="banner_nhahang">
-  <div class="s960">
-    <div id="ten_nha_hang">2014 Restaurant</div>
-    <div id="menu_wrapper"> 
-      <!--<div id="text_menu_chinh">-->
-      <ul id="menu">
-        <li><a href="#">Nhóm Nhà Hàng</a>
-          <ul class="submenu">
-            <li><a href="#">Nhà Hàng Buffet</a></li>
-            <li><a href="#">Nhà Hàng Truyền Thống</a></li>
-            <li><a href="#">Nhà Hàng Tiệc Cưới</a></li>
-          </ul>
-        </li>
-        <li><a href="#">Nhóm Món Ăn</a>
-          <ul class="submenu">
-            <li><a href="#">Lẩu</a></li>
-            <li><a href="#">Hải Sản</a></li>
-            <li><a href="#">Đồ Ăn Nhanh</a></li>
-            <li><a href="#">Cổ Truyền</a></li>
-          </ul>
-        </li>
-      </ul>
-      <!--</div>--> 
-    </div>
-  </div>
+	<div class="s960">
+		<div id="ten_nha_hang">2014 Restaurant</div>
+		<div id="menu_wrapper">
+			<!--<div id="text_menu_chinh">-->
+			<ul id="menu">
+				<li><a href="#">Nhóm Nhà Hàng</a>
+					<ul class="submenu">
+						<li><a href="#">Nhà Hàng Buffet</a></li>
+						<li><a href="#">Nhà Hàng Truyền Thống</a></li>
+						<li><a href="#">Nhà Hàng Tiệc Cưới</a></li>
+					</ul></li>
+				<li><a href="#">Nhóm Món Ăn</a>
+					<ul class="submenu">
+						<li><a href="#">Lẩu</a></li>
+						<li><a href="#">Hải Sản</a></li>
+						<li><a href="#">Đồ Ăn Nhanh</a></li>
+						<li><a href="#">Cổ Truyền</a></li>
+					</ul></li>
+			</ul>
+			<!--</div>-->
+		</div>
+	</div>
 </div>
-<!--<div id="banner_monan" class="canhgiua"></div>--> 
+<!--<div id="banner_monan" class="canhgiua"></div>-->
